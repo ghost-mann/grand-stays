269f21752aaf0a4146d3a6e1db76d383fe655fa3
ghost-mann <iamaob8@protonmail.com>
2024-11-15 14:38:49 +0300

    feat: added react router-dom and routes in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+import Home from "./pages/Home.tsx"
+import About from "./pages/About.tsx"
+import Contact from "./pages/Contact.tsx"
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Router>
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<About />} />
+          <Route path="/" element={<Contact />} />
+          </Routes>
+      </Router>
+
   </StrictMode>,
 )
